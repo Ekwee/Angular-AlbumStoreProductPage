cf884a2af79fc39ca1959bb7043e93c667d311ad
ryan ekwee <49526240+Ekwee@users.noreply.github.com>
2021-12-09 17:25:16 +0300

    interface manenoz

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
-
+import { Album } from './album';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ProductService {
@@ -12,9 +13,9 @@ export class ProductService {
 
   constructor(private _http : Http ) { }
 
-  getAlbum(id:number){
+  getAlbum(id:number) : Observable<Album>{
    return this._http.get(this._albumUrl)
-    .map((response =>
+    .map((response=>
       response.json()));
     
     
