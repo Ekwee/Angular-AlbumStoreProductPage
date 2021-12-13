5c71b9e9a19f4202eed4ebd2b0304b64036859b0
ryan ekwee <49526240+Ekwee@users.noreply.github.com>
2021-12-13 10:46:57 +0300

    fixed

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 import { Album } from '../album';
 import { ProductService } from '../product.service';
 
@@ -8,15 +7,15 @@ import { ProductService } from '../product.service';
   templateUrl: './product-tracklisting.component.html',
   styleUrls: ['./product-tracklisting.component.css']
 })
-export class ProductTracklistringComponent implements OnInit {
+export class ProductTracklistingComponent implements OnInit {
   albumInfo : Album;
-;
+
   constructor(private _productService: ProductService ) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(
       response => this.albumInfo = response
-    )
+    );
   }
 
 }
