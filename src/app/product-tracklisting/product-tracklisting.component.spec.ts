3acfbdcf1ad73879018cdd72522a6c10ef968a4a
ryan ekwee <49526240+Ekwee@users.noreply.github.com>
2021-12-13 10:22:53 +0300

    listing change

--- src/app/product-tracklisting/product-tracklisting.component.spec.ts
+++ src/app/product-tracklisting/product-tracklisting.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductTracklistringComponent } from './product-tracklistring.component';
+import { ProductTracklistringComponent } from './product-tracklisting.component';
 
 describe('ProductTracklistringComponent', () => {
   let component: ProductTracklistringComponent;
